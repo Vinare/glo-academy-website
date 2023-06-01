8bbdf9765b8a295405a2a87849a2c457e198d0dc
Vinare <scrat-me@mail.ru>
2023-06-01 21:54:43 +0300

    video block adapted + mobile container modified

--- js/main.js
+++ js/main.js
@@ -2,30 +2,30 @@
 const progressCircle = document.querySelector(".autoplay-progress svg");
 const progressContent = document.querySelector(".autoplay-progress span");
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 'auto',
-    loop: true,
-    spaceBetween: 40,
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 10,
 
-    breakpoints: {
-        576: {
-            slidesPerView: 'auto',
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 'auto',
-            spaceBetween: 60,
-        },
-        992: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-        },
-        1200: {
-            slidesPerView: 5,
-            spaceBetween: 10,
-        },
-        1400: {
-            slidesPerView: 5,
-            spaceBetween: 60,
-        }
-    }
+  breakpoints: {
+    480: {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: "3",
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 28,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    1400: {
+      slidesPerView: 5,
+      spaceBetween: 60,
+    },
+  },
 });
