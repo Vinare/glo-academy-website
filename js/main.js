454c4dc4e089feaed7ecd535bf22674ad5e857b7
Vinare <scrat-me@mail.ru>
2023-06-07 23:38:07 +0300

    professions section finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 //Swiper-slider
-const progressCircle = document.querySelector(".autoplay-progress svg");
-const progressContent = document.querySelector(".autoplay-progress span");
+
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: "auto",
   loop: true,
@@ -26,6 +25,37 @@ const swiper = new Swiper(".mySwiper", {
     1360: {
       slidesPerView: 5,
       spaceBetween: 20,
+    },
+  },
+});
+
+//Swiper-slider Professions
+
+const professionsSwiper = new Swiper(".professionsSwiper", {
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 10,
+
+  breakpoints: {
+    480: {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: "3",
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 28,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    1360: {
+      slidesPerView: 3,
+      spaceBetween: 21,
     },
   },
 });
