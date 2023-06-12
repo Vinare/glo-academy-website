8354729ae534d1d252c942d7815f3199225d93b5
Velmoren <velmoren@yahoo.com>
2023-06-12 23:21:04 +0300

    small fix

--- js/main.js
+++ js/main.js
@@ -4,13 +4,15 @@ const swiper = new Swiper(".mySwiper", {
   slidesPerView: "auto",
   loop: true,
   spaceBetween: 10,
-
+  slidesOffsetBefore: 16,
   breakpoints: {
     480: {
       slidesPerView: "auto",
+      slidesOffsetBefore: 16,
       spaceBetween: 20,
     },
     768: {
+      slidesOffsetBefore: 0,
       slidesPerView: "3",
       spaceBetween: 30,
     },
@@ -35,22 +37,24 @@ const professionsSwiper = new Swiper(".professionsSwiper", {
   slidesPerView: "auto",
   loop: true,
   spaceBetween: 10,
-
+  slidesOffsetBefore: 16,
   breakpoints: {
     480: {
       slidesPerView: "auto",
       spaceBetween: 20,
+      slidesOffsetBefore: 16,
     },
     768: {
-      slidesPerView: "3",
+      slidesPerView: "auto",
       spaceBetween: 30,
+      slidesOffsetBefore: 0,
     },
     992: {
-      slidesPerView: 4,
+      slidesPerView: "auto",
       spaceBetween: 28,
     },
     1200: {
-      slidesPerView: 5,
+      slidesPerView: "auto",
       spaceBetween: 10,
     },
     1360: {
