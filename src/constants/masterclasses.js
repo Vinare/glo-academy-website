17a22db6b1ef5e2240788f032acf67e9db869b31
Velmoren <velmoren@yahoo.com>
2023-10-16 20:53:55 +0300

    add masterclasses js

---
+++ src/constants/masterclasses.js
@@ -0,0 +1,57 @@
+export const masterclasses = [
+    {
+        id: 0,
+        title: 'Промосайт к фильму «Веном 2»',
+        image: 'https://glo-academy.ru/wp-content/uploads/2021/06/intensive-card-preview.jpg',
+        link: 'https://glo-academy.ru/intensive/venom/',
+        tags: ['HTML / CSS'],
+        time: 122
+    },
+    {
+        id: 1,
+        title: 'Сервис доставки еды на HTML и CSS',
+        image: 'https://glo-academy.ru/wp-content/uploads/2021/10/intensive-card-preview-2.jpg',
+        link: 'https://glo-academy.ru/intensive/delivery-service/',
+        tags: ['HTML / CSS'],
+        time: 130
+    },
+    {
+        id: 2,
+        title: 'Свой Avito на HTML и CSS',
+        image: 'https://glo-academy.ru/wp-content/uploads/2020/07/intensive-card-preview.jpg',
+        link: 'https://glo-academy.ru/intensive/vorkshop-svoy-avito-na-javascript/',
+        tags: ['HTML / CSS'],
+        time: 150
+    },
+    {
+        id: 4,
+        title: 'Свой Youtube на HTML и CSS и безудержном желании оторваться',
+        image: 'https://glo-academy.ru/wp-content/uploads/2021/04/intensive-card-preview.jpg',
+        link: 'https://glo-academy.ru/intensive/youtube/',
+        tags: ['HTML / CSS'],
+        time: 171
+    },
+    {
+        id: 4,
+        title: 'Своя фриланс-биржа на JavaScript',
+        image: 'https://glo-academy.ru/wp-content/uploads/2020/06/freelance.jpg',
+        link: 'https://glo-academy.ru/intensive/svoya-frilans-birzha-na-javascript/',
+        tags: ['JS'],
+        time: 615
+    },
+    {
+        id: 5,
+        title: 'Сокращатель ссылок на PHP',
+        image: 'https://glo-academy.ru/wp-content/uploads/2021/10/intensive-card-preview-3.jpg',
+        link: 'https://glo-academy.ru/intensive/short-link/',
+        tags: ['PHP'],
+        time: 212
+    }
+]
+
+export const hovers = [
+    '-lavender',
+    '-blue',
+    '-lavender-lilac',
+    '-dark-blue'
+]
