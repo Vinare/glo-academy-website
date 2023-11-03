f69db6d2ad85581ff2a0292639929b55e710efa4
Vinare <scrat-me@mail.ru>
2023-11-03 19:39:19 +0300

    students section adaptive for 991

--- src/modules/sliderInit.js
+++ src/modules/sliderInit.js
@@ -183,64 +183,6 @@ export const sliderInit = () => {
 
   breakpointChecker();
 
-  // Swiper-slider Students
-  // const studentsSwiper = new Swiper(".studentsSwiper", {
-  //   loop: false,
-  //   modules: [ Autoplay ],
-  //   autoplay: {
-  //     delay: 2500,
-  //     disableOnInteraction: false,
-  //   },
-  //   slidesPerView: "auto",
-  //   spaceBetween: 10,
-  //   // slidesOffsetBefore: 16,
-  //   // slidesOffsetAfter: 16,
-  //   // speed: 500,
-  //   // breakpoints: {
-  //   //   480: {
-  //   //     // slidesPerView: "auto",
-  //   //     spaceBetween: 20,
-  //   //     slidesOffsetBefore: 16,
-  //   //     slidesOffsetAfter: 12,
-  //   //   },
-  //   //   576: {
-  //   //     // slidesPerView: "auto",
-  //   //     spaceBetween: 22,
-  //   //     slidesOffsetBefore: 30,
-  //   //     slidesOffsetAfter: 30,
-  //   //   },
-  //   //   768: {
-  //   //     // slidesPerView: 2,
-  //   //     spaceBetween: 31,
-  //   //     slidesOffsetBefore: 0,
-  //   //     slidesOffsetAfter: 0,
-  //   //   },
-  //   //   992: {
-  //   //     // slidesPerView: 3,
-  //   //     spaceBetween: 40,
-  //   //     slidesOffsetBefore: 0,
-  //   //   },
-  //   //   1200: {
-  //   //     // slidesPerView: 3,
-  //   //     spaceBetween: 6,
-  //   //     slidesOffsetBefore: 0,
-  //   //   },
-  //   //   1360: {
-  //   //     // slidesPerView: 3,
-  //   //     spaceBetween: 13,
-  //   //     slidesOffsetBefore: 0,
-  //   //   },
-  //   //   1400: {
-  //   //     // slidesPerView: 3,
-  //   //     spaceBetween: 103,
-  //   //     slidesOffsetBefore: 0,
-  //   //   },
-  //   // },
-
-  //   // keyboard: {
-  //   //   enabled: true,
-  //   // },
-  // });
 
   // Splide-slider Students
 
@@ -252,6 +194,11 @@ export const sliderInit = () => {
     arrows: false,
     pagination: false,
     // autoplay: true,
+    breakpoints: {
+      1199: {
+        gap: 37,
+      },
+    },
   });
 
   splide.mount();
